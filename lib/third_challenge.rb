074f4607b6367a96f7a4a498ed1189d37e895c96
Nia Bert <nbert14@gmail.com>
2019-05-25 00:18:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,26 +1,26 @@
 def third_challenge
-  epic_tragedy = {	  epic_tragedy = {
-   :montague => {	   :montague => {
-      :patriarch => {	      :patriarch => {:name => "Lord Montague", :age => "53"},
-        #your key/value pairs here	      :matriarch => { :name => "Lady Montague", :age => "54"},
-        },	      :hero => { :name => "Romeo", :age => "15", :status => "alive"
-      :matriarch => {	
-        #your key/value pairs here	
-        },	
-      :hero => {	
-        #your key/value pairs here	        #your key/value pairs here
-        },	        },
-      :hero_friends => []	      :hero_friends => []
-   }, 	   }, 
-   :capulet => {	   :capulet => {
-      :patriarch => {	      :patriarch => { :name => "Lord Capulet", :age => "50"
-        #your key/value pairs here	        #your key/value pairs here
-        },	        },
-      :matriarch => {	      :matriarch => { :name => "Lady Capulet", :age => "51"
-        #your key/value pairs here	        #your key/value pairs here
-        },	        },
-      :heroine => {	      :heroine => { :name => "Juliet", :age => "15", :status => "alive"
-        #your key/value pairs here	        #your key/value pairs here
-        },	        },
-      :heroine_friends => []	      :heroine_friends => []
-      
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :matriarch => { :name => "Lady Montague", :age => "54"},
+      :hero => { :name => "Romeo", :age => "15", :status => "alive"
+        #your key/value pairs here
+        },
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => { :name => "Lord Capulet", :age => "50"
+        #your key/value pairs here
+        },
+      :matriarch => { :name => "Lady Capulet", :age => "51"
+        #your key/value pairs here
+        },
+      :heroine => { :name => "Juliet", :age => "15", :status => "alive"
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+   }
+  }
+
+  
+end
