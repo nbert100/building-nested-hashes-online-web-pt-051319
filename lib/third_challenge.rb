95ea0e234bdae53d12babf054086f23e13ff216b
Nia Bert <nbert14@gmail.com>
2019-05-25 00:17:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,25 @@
 def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
-
-  
-end
+  epic_tragedy = {	  epic_tragedy = {
+   :montague => {	   :montague => {
+      :patriarch => {	      :patriarch => {:name => "Lord Montague", :age => "53"},
+        #your key/value pairs here	      :matriarch => { :name => "Lady Montague", :age => "54"},
+        },	      :hero => { :name => "Romeo", :age => "15", :status => "alive"
+      :matriarch => {	
+        #your key/value pairs here	
+        },	
+      :hero => {	
+        #your key/value pairs here	        #your key/value pairs here
+        },	        },
+      :hero_friends => []	      :hero_friends => []
+   }, 	   }, 
+   :capulet => {	   :capulet => {
+      :patriarch => {	      :patriarch => { :name => "Lord Capulet", :age => "50"
+        #your key/value pairs here	        #your key/value pairs here
+        },	        },
+      :matriarch => {	      :matriarch => { :name => "Lady Capulet", :age => "51"
+        #your key/value pairs here	        #your key/value pairs here
+        },	        },
+      :heroine => {	      :heroine => { :name => "Juliet", :age => "15", :status => "alive"
+        #your key/value pairs here	        #your key/value pairs here
+        },	        },
+      :heroine_friends => []	      :heroine_friends => []
